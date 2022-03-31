a518e9fa478692645b04f372ddb5a20ed9a7471e
Marcelle Mendes <62451375+marcelleemendess@users.noreply.github.com>
2022-03-31 16:36:05 -0600

    simple carousel with react

---
+++ src/components/SliderData.js
@@ -0,0 +1,17 @@
+export const SliderData = [
+    {
+        image: 'https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1544991337-95176b5233c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1544989164-31dc3c645987?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1564659907532-6b5f98c8e70f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1558732314-f2ee01b9ee51?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2074&q=80'
+    }
+]   
